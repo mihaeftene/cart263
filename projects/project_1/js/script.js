c2acc78fef09b1854dd5fa3551e5c8868451c61d
mihaeftene <mihaeftene@users.noreply.github.com>
2020-02-06 13:28:31 -0500

    P1: After a certain amount of character written on the paper area, 4 characters will be deleted everytime.

--- projects/project_1/js/script.js
+++ projects/project_1/js/script.js
@@ -16,9 +16,19 @@ let essayQuestionsArray = ['An Analysis of the True Hero of A Lesson Before Dyin
 /*what will show when the page is loaded*/
 $(document).ready(setup);
 function setup(){
-//set the timer for the questions when to change. In this case it will change every 10 seconds. Good luck! :)
-setInterval(showNewEssayQuestion,10000);
+  //set the timer for the questions when to change. In this case it will change every 10 seconds. Better work that brain! Good luck! :)
+setInterval(showNewEssayQuestion,1000);
 //console.log(document.getElementById("text"));
+
+//for the text area where the user will write its essay, After a certain number of characters written (20), the characters will be slowly deleted by 4.
+$("#text").on("change keyup paste", function() {
+  console.log( $(this).val());
+  //if the number of characters is bigger than 20, then delete 4 characters.
+  if($(this).val().length>20){
+  let characterString = $(this).val().substring(0,$(this).val().length-4);
+  $(this).val(characterString);
+}
+});
 }
 
 //showNewEssayQuestion()-->Meant for triggering a question once the button has been clicked.
