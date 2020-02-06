e1d47137f04da7b160ae93ac579fe8de34ad9931
mihaeftene <mihaeftene@users.noreply.github.com>
2020-02-06 17:26:48 -0500

    P1: Added the typing sound and fixed the delete character code to match the sound

--- projects/project_1/js/script.js
+++ projects/project_1/js/script.js
@@ -19,9 +19,13 @@ let $paper;
 //declare the liquidPaper variable
 let $liquidPaper;
 
+//global variable
+let deletingLetters;
+
 // Sound effects for the experience
 let trollSFX = new Audio("assets/sounds/trollSong.mp3");
 let deleteAllSFX = new Audio("assets/sounds/deleteAll.wav");
+let typingDeleteSFX = new Audio("assets/sounds/typingDelete.wav");
 
 /*what will show when the page is loaded*/
 $(document).ready(setup);
@@ -36,18 +40,23 @@ function setup() {
 
     //setting up the jquery character object that is being grabbed from html
     let $theCharacterString = $(this)
-    //if the number of characters is bigger than 50, then delete 1 character slowly.
-    if ($theCharacterString.val().length > 50) {
-      let deletingLetters = setInterval(function() {
-        let letterString = $theCharacterString.val().substring(0, $theCharacterString.val().length - 1);
-        $theCharacterString.val(letterString); //letters string
-        //if statement. Once the number of characters is 0 clear the interval.
-        if ($theCharacterString.val().length === 0) {
-          clearInterval(deletingLetters);
-        }
-        //delete the characters every 1 second
-      }, 1000)
+    //if the number of characters is equal 50, then delete 1 character slowly.
+    if ($theCharacterString.val().length === 50) {
+      console.log("Oh no");
+      if (deletingLetters === undefined) {
+        deletingLetters = setInterval(function() {
+            let letterString = $theCharacterString.val().substring(0, $theCharacterString.val().length - 1);
+            $theCharacterString.val(letterString); //letters string
+            typingDeleteSFX.play();
+            console.log("delete");
+            //if statement. Once the number of characters is 0 clear the interval.
+            if ($theCharacterString.val().length === 0) {
+              clearInterval(deletingLetters);
+            }
+            //delete the characters every 300 mil
+          }, 300)
     }
+  }
   });
 
   //lets do the paint!
@@ -58,22 +67,21 @@ function setup() {
     // The drop option specifies a function to call when a drop is completed
     drop: onDrop
   });
-  //make the liquid paper draggable
+  //make the liquid paper draggable and play the delete song
   $("#dropLiquidPaper").draggable({
     start: function() {
       deleteAllSFX.play();
     },
     stop: function() {
-    trollSFX.pause();
+      trollSFX.pause();
     }
   });
-  //make the red paint draggable
+  //make the red paint draggable and play the troll song
   $("#dropRedPaint").draggable({
     start: function() {
       trollSFX.play();
     },
-    stop: function(){
-    }
+    stop: function() {}
   });
 }
 
@@ -89,14 +97,14 @@ function onDrop(event, ui) {
   // ui contains a reference to the draggable element that was just dropped in ui.draggable
   // .remove() removes the select element from the page
   //check which id we are using. It allows to change between the liquid paper or the red paper.
-  let id =ui.draggable.attr('id');
+  let id = ui.draggable.attr('id');
   //if the paper is clean and we are using the red paint, remove the clean paper and add the red paper
-  if (id === "dropRedPaint"){
+  if (id === "dropRedPaint") {
     $("#text").removeClass("cleanPaper");
     $("#text").addClass("redPaper");
   }
   //else, if we use the liquidPaper, remove the red paper and replace it with clean paper.
-  else if (id === "dropLiquidPaper"){
+  else if (id === "dropLiquidPaper") {
     $("#text").removeClass("redPaper");
     $("#text").addClass("cleanPaper");
   }
