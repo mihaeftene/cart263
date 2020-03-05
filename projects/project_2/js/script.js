ebfe9c94fc78c3e0f365953a314ae3b03b38363d
mihaeftene <mihaeftene@users.noreply.github.com>
2020-03-05 09:32:34 -0500

    P2: Started Project 2 using the jquery template from pippin's website.

---
+++ projects/project_2/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/********************************************************************
+
+Name of the Project
+Mihaela Eftene
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
+
+$(document).ready(setup);
+
+
+function setup() {
+
+}
